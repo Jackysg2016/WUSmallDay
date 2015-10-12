fecca30eb2df91396453320a6ce9f66f3aa552f7
wuqihan <wuqihan123@163.com>
2015-10-12 09:56:57 +0800

    init

---
+++ SmallDay/Base/WUBaseViewController.h
@@ -0,0 +1,13 @@
+    //
+//  WUBaseViewController.h
+//  SmallDay
+//
+//  Created by wuqh on 15/10/12.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WUBaseViewController : UIViewController
+
+@end
