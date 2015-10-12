de73b0430a75d0d0ef358b6d9654d610c84346e5
wuqihan <wuqihan123@163.com>
2015-10-12 09:40:36 +0800

    init

---
+++ SmallDay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmallDay
+//
+//  Created by wuqh on 15/10/10.
+//  Copyright © 2015年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
